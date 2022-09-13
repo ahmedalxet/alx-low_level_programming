4c566053ec8184b8dbaeea5bb5af00c3da45d498
ahmedalxet <ahmelaq@gmail.com>
2022-09-13 09:46:46 +0300

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+/**
+ * main - Prints _putchar
+ * @void: Empty parameter list for main.
+ *
+ * Description: Prints _putchar
+ * 
+ *
+ * Return: 0 for success
+*/
+
+#include <stdio.h>
+
+
+int main() {
+    printf("_putchar");
+
+    return 0;
+}
