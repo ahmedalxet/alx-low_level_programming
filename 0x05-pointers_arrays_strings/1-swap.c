33da8c0e357a8f7e7e5f823e5a47be4f5cb6b783
ahmedalxet <ahmelaq@gmail.com>
2022-09-20 23:55:25 +0300

    mandatory tasks

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+include "main.h"
+
+/**
+ * reset_to_98 - a function that swaps the values of tow integers
+ * 
+ * @a: An integer input pointer
+ * @b: An integer input pointer
+ * Return: Nothing.
+ */
+void swap_int(int *a, int *b)
+{
+	int temp;
+	temp = *a;
+	*a = *b;
+	*b = temp
+
+}
