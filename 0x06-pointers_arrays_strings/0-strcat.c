6e3f64e8bc4c0922c83580a47e65481f253613c9
ahmedalxet <ahmelaq@gmail.com>
2022-09-21 15:56:52 +0300

    0-strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strcat - check the code
+ * @dest: destination
+ * @src: source
+ * Return: Concatinated text.
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
