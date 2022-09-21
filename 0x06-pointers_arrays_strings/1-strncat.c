8f7be8aa3da8400b227ec6b2b8f2bf6282b06364
ahmedalxet <ahmelaq@gmail.com>
2022-09-21 16:15:27 +0300

    strncpy

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strncat - check the code
+ * @dest: destination
+ * @src: source
+ * @n: int
+ * Return: Concatinated text.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	return (strncat(dest, src, n));
+}
---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strncpy - check the code
+ * @dest: destination
+ * @src: source
+ * @n: int
+ * Return: Concatinated text.
+ */
+char _strncpy(char *dest, char *src, int n)
+{
+	return (strncpy(dest, src, n));
+}
