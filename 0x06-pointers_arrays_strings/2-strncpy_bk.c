90f1e0be5117b29b85ecafadc901fef49bf5f50b
ahmedalxet <ahmelaq@gmail.com>
2022-09-22 10:11:17 +0300

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy_bk.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strncpy - copy a string
+ * @dest: destination
+ * @src: source
+ * @n: int
+ * Return: Concatinated text.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int srclen = 0, i = 0;
+	char *temp = dest, *start = src;
+
+	while (*src)
+	{
+		srclen++;
+		src++;
+	}
+
+	srclen++;
+
+	if (n > srclen)
+		n = srclen;
+
+	src = start;
+
+	for (; i < n; i++)
+		*dest++ = *src++;
+
+	return (temp);
+}
