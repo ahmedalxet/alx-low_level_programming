26dd2070cbb9a089e395058fff106af1ac80541d
ahmedalxet <ahmelaq@gmail.com>
2022-09-22 11:18:11 +0300

    correction on 100

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -2,7 +2,7 @@
 
 /**
  * rot13 -  a function that encodes a string using rot13.
- * @s: An input string to encode using rot13
+ * @str: An input string to encode using rot13
  * Return: An encode string
  */
 char *rot13(char *str)
